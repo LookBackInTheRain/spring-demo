f9db662fb99b0cd8d4b0c511cd3a45d6ec71a04e
yuit <1239964852g@gmail.com>
2019-12-01 21:16:45 +0800

    commit

---
+++ src/main/java/club/yuit/Main.java
@@ -0,0 +1,13 @@
+package club.yuit;
+
+/**
+ * @author yuit
+ * @date 2019-12-01 21:05
+ */
+public class Main {
+
+  public static void main(String[] args){
+
+  }
+
+}
---
+++ src/test/java/club/yuit/MainTest.java
@@ -0,0 +1,21 @@
+package club.yuit;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * @author yuit
+ * @date 2019-12-01 21:03
+ */
+@RunWith(JUnit4.class)
+@Slf4j
+public class MainTest {
+
+  @Test
+  public void mainTest(){
+    log.info("Test");
+  }
+
+}
